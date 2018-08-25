7bc49dbcda29fa8b435c0391e7b0009a143c69a5
Keir Bowden <keirbowden@brightgen.com>
2018-08-25 12:59:19 +0100

    Created.

---
+++ src/aura/DemoEvents/DemoEventsController.js
@@ -0,0 +1,32 @@
+({
+    init : function(component, event, helper) {
+        var empApi = component.find("empApi");
+        
+        // Error handler function that prints the error to the console.
+        var errorHandler = function (message) {
+            console.log("Received error ", message);
+        }.bind(this);
+        
+        // Register error listener and pass in the error handler function.
+        empApi.onError(errorHandler);
+        
+        var channel='/event/Demo_Event__e';
+        var sub;
+        
+        // new events
+        var replayId=-1;
+        
+        var callback = function (message) {
+			component.find('notifLib').showToast({
+            	"title": "Message Received!",
+	            "message": message.data.payload.Message__c
+        		});        
+        }.bind(this);
+
+        empApi.subscribe(channel, replayId, callback).then(function(value) {
+            console.log("Subscribed to channel " + channel);
+            sub = value;
+            component.set("v.sub", sub);
+        });
+    }
+ })
